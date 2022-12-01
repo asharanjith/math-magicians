233180e247c53e3d3ea7fa553f3162e260a75e10
asharanjith <ashasvijayan@gmail.com>
2022-12-01 16:56:05 +0400

    Refactor calculator component to functional

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,52 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: 0, operation: null, next: null };
-    this.clickEvent = this.clickEvent.bind(this);
-  }
+const Calculator = () => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { total: 0, operation: null, next: null };
+  //   this.clickEvent = this.clickEvent.bind(this);
+  // }
 
-  clickEvent = (e) => {
-    this.setState((state) => calculate(state, e.target.textContent));
+  const [state, setState] = useState({ total: 0, operation: null, next: null });
+  const { total, operation, next } = state;
+  const clickEvent = (e) => {
+    setState((state) => calculate(state, e.target.textContent));
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-
-      <div className="calculator-display">
-        <div className="calculator-display__result">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div className="calculator-keypad">
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>AC</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>+/-</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>%</button>
-          <button type="button" className="calculator-keypad__key operator" onClick={this.clickEvent}>÷</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>7</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>8</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>9</button>
-          <button type="button" className="calculator-keypad__key operator" onClick={this.clickEvent}>x</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>4</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>5</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>6</button>
-          <button type="button" className="calculator-keypad__key operator" onClick={this.clickEvent}>-</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>1</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>2</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>3</button>
-          <button type="button" className="calculator-keypad__key operator" onClick={this.clickEvent}>+</button>
-          <button type="button" className="calculator-keypad__key zero" onClick={this.clickEvent}>0</button>
-          <button type="button" className="calculator-keypad__key" onClick={this.clickEvent}>.</button>
-          <button type="button" className="calculator-keypad__key operator" onClick={this.clickEvent}>=</button>
-        </div>
+  return (
+    <div className="calculator-display">
+      <div className="calculator-display__result">
+        {total}
+        {operation}
+        {next}
       </div>
+      <div className="calculator-keypad">
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>AC</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>+/-</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>%</button>
+        <button type="button" className="calculator-keypad__key operator" onClick={clickEvent}>÷</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>7</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>8</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>9</button>
+        <button type="button" className="calculator-keypad__key operator" onClick={clickEvent}>x</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>4</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>5</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>6</button>
+        <button type="button" className="calculator-keypad__key operator" onClick={clickEvent}>-</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>1</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>2</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>3</button>
+        <button type="button" className="calculator-keypad__key operator" onClick={clickEvent}>+</button>
+        <button type="button" className="calculator-keypad__key zero" onClick={clickEvent}>0</button>
+        <button type="button" className="calculator-keypad__key" onClick={clickEvent}>.</button>
+        <button type="button" className="calculator-keypad__key operator" onClick={clickEvent}>=</button>
+      </div>
+    </div>
 
-    );
-  }
-}
+  );
+};
 
 export default Calculator;
