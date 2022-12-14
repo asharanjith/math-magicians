9e01e1f28e494b2bc15f21afe9a691ebc022d090
asharanjith <ashasvijayan@gmail.com>
2022-12-14 17:37:44 +0400

    Add more edge case to calculate.js

--- src/__Test__/calculate.test.js
+++
@@ -1,9 +0,0 @@
-import calculate from '../logic/calculate';
-
-describe('calculate', () => {
-  test('check AC button', () => {
-    const state = { total: 0, operation: null, next: null };
-    const buttonName = 'AC';
-    expect(calculate(state, buttonName)).toEqual({ total: 0, operation: null, next: null });
-  });
-});
